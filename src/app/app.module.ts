e4422a60f6cfe0ee879aa08a5f1cfb4052dcb769
kiady11 <kiady.rakotoharinjatovo@yahoo.com>
2022-06-22 19:31:51 +0300

    squelette home-page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavBarComponent } from './components/nav-bar/nav-bar.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { HomeComponent } from './components/pages/home/home.component';
+import { CardsComponent } from './components/cards/cards.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavBarComponent
+    NavBarComponent,
+    FooterComponent,
+    HomeComponent,
+    CardsComponent
   ],
   imports: [
     BrowserModule,
